966142726502a26d98454299cb39a375ea9fc11e
PRASAD759 <138235369+PRASAD759@users.noreply.github.com>
2024-01-05 23:00:55 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$conn=new mysqli('localhost','root','','crudoperation2');
+
+if(!$conn){
+echo "connection not Successfull";
+}
+?>
---
+++ update.php
@@ -0,0 +1,87 @@
+<?php
+include 'connect.php';
+$id=$_GET['updateid'];
+$sql="select * from `crud2` where id=$id";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_assoc($result);
+$name=$row['name'];
+$mobile=$row['mobile'];
+$email=$row['email'];
+$password=$row['password'];
+
+if(isset($_POST['submit'])){
+   $name=$_POST['name'];
+   $mobile=$_POST['mobile'];
+   $email=$_POST['email'];
+   $password=$_POST['password'];
+   $sql="update `crud2` set id=$id,name='$name',mobile='$mobile',email='$email',password='$password' where id=$id";
+   $result=mysqli_query($conn,$sql);   
+   
+   if($result){
+      echo"data updated successfully";  
+      header('location:display.php');
+       }
+      else{
+         die(mysqli_error($conn));
+      }
+
+   }  
+    ?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Crud Operation Using php</title>
+  </head>
+  <body>
+   <div class="container my-5">
+ 
+
+   <form method="post">
+  <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off" value=<?php
+    echo $name;
+    ?>>
+ </div>
+
+
+ <div class="form-group">
+    <label>Mobile</label>
+    <input type="text" class="form-control" placeholder="Enter your your mobile" name="mobile" autocomplete="off" value=<?php echo $mobile; ?>>
+ </div>
+
+ <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off" value=<?php echo $email; ?>>
+ </div>
+
+
+ <div class="form-group">
+    <label>Password</label>
+    <input type="text" class="form-control" placeholder="Enter your Password" name="password" autocomplete="off" value=<?php echo $password; ?>>
+ </div>
+
+
+
+
+
+  <button type="submit" class="btn btn-primary" name="submit">Update</button>
+</form>
+
+
+
+
+   </div>
+
+
+  </body>
+</html>
